c3d87e6dce6a65f3778954a2d8e10ddd4722a32f
Sami Shaio <sami@flipboard.com>
2018-12-07 15:55:31 -0800

    part 2 solution

--- 2018/day07/day07.java
+++ 2018/day07/day07.java
@@ -6,11 +6,57 @@ public class day07 {
     static Pattern inputPattern = Pattern.compile("Step (.) must be finished before step (.) can begin.");
     static Map<String, Node> map = new HashMap<String, Node>();
     static List<Node> list = new ArrayList<Node>();
+    static List<Worker> workers = new ArrayList<Worker>();
+    static int timeDelay = 60;
+
+    static class Worker implements Comparator<Worker> {
+        Node task;
+        int startTime;
+
+        Worker() {
+        }
+
+        public int compare(Worker e1, Worker e2) {
+            if (e1.task == null) {
+                return -1;
+            } else if (e2.task == null) {
+                return 1;
+            } else {
+                return e1.task.label.compareTo(e2.task.label);
+            }
+        }
+
+        void assign(Node task, int time) {
+            this.task = task;
+            this.startTime = time;
+            task.assigned = true;
+        }
+
+        boolean isDone(int time, StringBuilder result) {
+            if (task == null) {
+                return false;
+            }
+
+            if ((startTime + task.duration()) == time) {
+                task.output(result);
+                task = null;
+                startTime = 0;
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        boolean isAssigned() {
+            return task != null;
+        }
+    }
 
     static class Node implements Comparator<Node> {
         String label;
         List<String> dependencies = new ArrayList<String>();
         boolean finished;
+        boolean assigned;
 
         Node(String label) {
             this.label = label;
@@ -32,12 +78,16 @@ public class day07 {
             }
         }
 
+        int duration() {
+            return (label.charAt(0) - 'A') + 1 + timeDelay;
+        }
+
         boolean isFinished() {
             return finished;
         }
 
         boolean available() {
-            if (finished) {
+            if (finished || assigned) {
                 return false;
             } else {
                 for (String l : dependencies) {
@@ -51,10 +101,16 @@ public class day07 {
                 return true;
             }
         }
+        
+        boolean inProgress() {
+            return assigned;
+        }
 
         void output(StringBuilder output) {
-            finished = true;
-            output.append(label);
+            if (! finished) {
+                finished = true;
+                output.append(label);
+            }
         }
 
         public String toString() {
@@ -108,9 +164,68 @@ public class day07 {
         return sb.toString();
     }
 
+    static int scheduledOutput(int nworkers, int delay) {
+        StringBuilder result = new StringBuilder();
+
+        timeDelay = delay;
+        for (int i=0; i < nworkers; i++) {
+            workers.add(new Worker());
+        }
+
+        boolean workLeft;
+        int time = 0;
+
+        do {
+            workLeft = false;
+
+            int assignedWorkers = 0;
+
+            for (Worker w : workers) {
+                if (w.isAssigned() && !w.isDone(time, result)) {
+                    workLeft = true;
+                    assignedWorkers++;
+                }
+            }
+
+            workers.sort(workers.get(0));
+
+            for (Node n : list) {
+                if (assignedWorkers == workers.size()) {
+                    break;
+                }
+
+                if (!n.inProgress() && n.available()) {
+                    workLeft = true;
+                    boolean assignedWorker = false;
+                    for (Worker w : workers) {
+                        if (! w.isAssigned()) {
+                            w.assign(n, time);
+                            assignedWorkers++;
+                            assignedWorker = true;
+                            break;
+                        }
+                    }
+                    if (assignedWorker) {
+                        workers.sort(workers.get(0));                   
+                    }
+                }
+            }
+            time++;
+        } while (workLeft);
+
+        return time - 1;
+    }
+
     public static void main(String args[]) {
         getInput((args.length==0) ? "input.txt" : args[0]);
 
         System.out.println(buildOutput());
+
+        for (Node l : list) {
+            l.finished = false;
+        }
+
+        System.out.println(scheduledOutput(((args.length > 1) ? Integer.parseInt(args[1]) : 5), 
+                                           ((args.length >= 2) ? Integer.parseInt(args[2]) : 60)));
     }
 }
