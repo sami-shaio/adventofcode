1efe34a787493583f87321ec87c9685ac75025d4
Sami Shaio <sami@flipboard.com>
2018-12-08 01:23:42 -0800

    part 2 solution

--- 2018/day08/day08.java
+++ 2018/day08/day08.java
@@ -2,6 +2,61 @@ import java.util.*;
 import java.io.*;
 
 public class day08 {
+    static class Node {
+        List<Node> children = new ArrayList<Node>();
+        List<Integer> metadata = new ArrayList<Integer>();
+        int start;
+
+        int sumMetadata() {
+            int sum = 0;
+            for (Integer m : metadata) {
+                sum += m;
+            }
+            for (Node n : children) {
+                sum += n.sumMetadata();
+            }
+
+            return sum;
+        }
+
+        int value() {
+            if (children.size() == 0) {
+                return sumMetadata();
+            } else {
+                int v = 0;
+                for (Integer m : metadata) {
+                    if (m == 0 || m > children.size()) {
+                        continue;
+                    }
+
+                    v += children.get(m - 1).value();
+                }
+
+                return v;
+            }
+        }
+
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+
+            sb.append("Node:");
+            for (Integer m : metadata) {
+                sb.append(" "+m);
+            }
+            return sb.toString();
+        }
+
+        void print(int spaces) {
+            for (int i =0; i < spaces; i++) {
+                System.out.print(" ");
+            }
+            System.out.println(this);
+            for (Node n : children) {
+                n.print(spaces+5);
+            }
+        }
+    }
+
     static List<Integer> getInput(String file) {
         List<Integer> list = new ArrayList<Integer>();
 
@@ -22,33 +77,33 @@ public class day08 {
         return list;
     }
 
-    // return offset into next node, accumulata metadata in metadata
-    static int sumMetadata(List<Integer> input, int start, List<Integer> metadata) {
+    static Node readTree(List<Integer> input, int start) {
         int nchild = input.get(start);
         int nmeta = input.get(start+1);
+        Node r = new Node();
+        Node child = null;
 
-        start += 2;
-
+        r.start = start + 2;
         for (int i=0; i < nchild; i++) {
-            start = sumMetadata(input, start, metadata);
+            r.children.add(child = readTree(input, (i==0) ? r.start : child.start));
         }
 
         for (int i=0; i < nmeta; i++) {
-            metadata.add(input.get(start+i));
+            r.metadata.add(input.get((child == null) ? r.start+i : child.start+i));
         }
+        
+        r.start = (child == null) ? r.start+nmeta : child.start + nmeta;
 
-        return start + nmeta;
+        return r;
     }
 
     public static void main(String args[]) {
         List<Integer> input = getInput((args.length==0) ? "input.txt" : args[0]);
         List<Integer> metadata = new ArrayList<Integer>();
+        Node root = readTree(input, 0);
 
-        sumMetadata(input, 0, metadata);
-        int sum = 0;
-        for (Integer m : metadata) {
-            sum += m;
-        }
-        System.out.println(sum);
+        //root.print(0);
+        System.out.println(root.sumMetadata());
+        System.out.println(root.value());
     }
 }
